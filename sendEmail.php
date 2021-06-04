bb147004c68bbddb980fd87142b994711d31e7f0
Supasin Saetang <supasin.s1t@gmail.com>
2021-06-04 11:30:26 +0700

    edit phpmailer

--- sendEmail.php
+++ sendEmail.php
@@ -73,7 +73,7 @@ if (isset($_POST['fname']) && isset($_POST['phone']) && isset($_POST['email']) &
     // SMTP Settings
     $mail->CharSet = 'UTF-8';
     $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
+    $mail->Host = "http://www.blackitch.com/";
     $mail->SMTPAuth = true;
     $mail->Username = "supasin.s1t@gmail.com"; // enter your email address
     $mail->Password = "fofyf17041998boybandfedfe"; // enter your password
